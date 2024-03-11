1e36257855bf0455b1ba179999d7e5015e4a0c07
Minhtuan <91857446+MinhTuan2405@users.noreply.github.com>
2024-03-11 11:36:46 +0700

    Add files via upload

---
+++ MANGSINHVIEN.cpp
@@ -0,0 +1,309 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <stack>
+#include <deque>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+#include <iterator>
+#include <iomanip>
+
+using std::string;
+using std::stack;
+using std::deque;
+#define vi std::vector<int>
+#define mp std::map <int, int>
+#define pi std::pair<int, int>
+#define endl std::cout << std::endl
+
+// prototype
+
+// in this class, define the class follow this instructation
+/*
+xây dựng các lớp sau kèm theo các thao tác của nó:
+- Lớp Điểm, gồm có các thuộc tính:
++ mảng số thực: lưu danh sách điểm các môn học (các điểm là các số thực >=0 và <=10)
++ số lượng phần tử của danh sách điểm.
+
+Thao tác: xây dựng các toán tử >>, <<, [], =, lấy điểm trung bình cộng, các phương thức khởi tạo (3 phương thức), phương thức hủy, các phương thức get và set.
+
+*/
+class Diem {
+    private:
+        int number;
+        double mark[20];
+
+        friend class SinhVien;
+    public:
+        Diem ();
+        Diem (int number_, double mark_[]);
+        Diem (Diem& grade);
+        friend std::istream& operator>> ( std::istream& in, Diem& grade){
+            grade.number = 0;
+            while ( in >> grade.mark[grade.number]){
+                ++grade.number;
+            }
+            return in;
+        }
+        friend std::ostream& operator<< ( std::ostream& out, Diem grade){
+             out << grade.number << '\n';
+            for ( int i = 0; i < grade.number; ++i){
+                 out << grade.mark[i] << " ";
+             }
+            return out;
+        };
+        void get();
+        int get_sub() {return number;};
+        void set();
+        double _GET_AVERAGE();
+        ~Diem();
+
+};
+
+Diem::Diem(){
+    number = 0;
+    for ( int i = 0; i < 20; ++i){
+        mark[i] = 0;
+    }
+}
+
+Diem::Diem(int number_, double mark_[]){
+    number = number_;
+    for ( int i = 0; i < number; ++i){
+        mark[i] = mark_[i];
+    }
+}
+
+Diem::Diem ( Diem& grade){
+    number = grade.number;
+    for ( int i = 0; i < number; ++i){
+        mark[i] = grade.mark[i];
+    }
+}
+
+void Diem::set(){
+    std::cin >> number;
+    for ( int i = 0; i < number; ++i){
+        std::cin >> mark[i];
+    }
+}
+
+void Diem::get(){
+    std::cout << number; endl;
+    for (int i = 0; i < number; ++i){
+        std::cout << mark[i] << " ";
+    }
+}
+
+double Diem::_GET_AVERAGE(){
+    double sum = 0 ;
+    for ( int i = 0; i < number; ++i){
+        sum += mark[i];
+    }
+    return (double) sum / number;
+}
+
+Diem::~Diem(){
+    number = 0;
+    for ( int i = 0; i < number; ++i){
+        mark[i] = 0;
+    }
+}
+
+/*
+Lớp SinhViên, gồm có các thuộc tính
++ Họ tên (chuỗi có khoảng trắng)
++ Mã Sinh Viên (chuỗi không có khoảng trắng)
++ Điểm (thuộc lớp điểm khai báo ở trên).
+Thao tác: xây dựng các toán tử >>, <<, =, < (dựa vào điểm trung bình),
+ lấy điểm trung bình cộng, các phương thức khởi tạo (3 phương thức), phương thức hủy, các phương thức get và set.
+*/
+
+class SinhVien {
+    private:
+        string fullname;
+        string code;
+        Diem grade;
+    
+    friend class MangSinhVien;
+    friend class Diem;
+    public:
+        SinhVien(){
+            fullname = "";
+            code = "";
+            grade.number = 0;
+            for ( int i = 0; i < grade.number; ++i){
+                grade.mark[i] = 0;
+            }
+        }
+
+        SinhVien (string fullname_, string code_, Diem grade_){
+            fullname = fullname_;
+            code = code_;
+            grade = grade_;
+        }
+
+        SinhVien ( SinhVien* sinhvien){
+            fullname =sinhvien -> fullname;
+            code = sinhvien -> code;
+            grade = sinhvien -> grade;
+        }
+
+        friend std::istream& operator>> ( std::istream& in, SinhVien& sinhvien ){
+            std::fflush (stdin);
+            std::getline ( in, sinhvien.fullname, '\n');
+            //in.ignore ('\n');
+            in >> sinhvien.code;
+            in >> sinhvien.grade;
+            return in;
+        }
+
+        friend std::ostream& operator<< ( std::ostream& out, SinhVien sinhvien){
+            out << "Ho Ten: " << sinhvien.fullname << '\n';
+            out << "Ma Sinh Vien: " << sinhvien.code << '\n';
+            out << "DTB: " << std::setprecision(1) << std::fixed << sinhvien.grade._GET_AVERAGE();
+            return out;
+        }
+
+        SinhVien operator= ( SinhVien sinhvien){
+            fullname = sinhvien.fullname;
+            code = sinhvien.code;
+            grade = sinhvien.grade;
+        }
+
+        bool operator< ( SinhVien sinhvien){
+            return grade._GET_AVERAGE() < sinhvien.grade._GET_AVERAGE();
+        }
+
+        void set (){
+            std::cin >> fullname;
+            std::cin >> code;
+            std::cin >> grade.number;
+            for ( int i = 0; i < grade.number; ++i){
+                std::cin >> grade.mark[i];
+            }
+        }
+
+        string get_fullname(){
+            return fullname;
+        }
+
+        string get_code (){
+            return code;
+        }
+
+        int get_sub (){
+            return grade.number;
+        }
+
+        double get_average_mark ( ){
+            return grade._GET_AVERAGE();
+        }
+
+        void get(){
+            std::cout << "Ho Ten: " << get_fullname() ; endl;
+            std::cout << "Ma Sinh Vien: " << get_code(); endl;
+            std::cout << "DTB: " << std::setprecision(1) << std::fixed << get_average_mark();
+        }
+        ~SinhVien (){
+            fullname = "";
+            code = "";
+            for ( int i = 0; i < grade.number; ++i) grade.mark[i] = 0;
+            grade.number = 0;
+        }
+
+};
+
+class MangSinhVien{
+    private:
+        int n;
+        SinhVien* sinhvien;
+    public:
+        MangSinhVien(){
+            n = 0;
+            sinhvien = NULL;
+        }
+
+        MangSinhVien(int n_){
+            n = n_;
+            sinhvien = new SinhVien[n];
+        }
+
+        MangSinhVien (MangSinhVien* mangsinhvien){
+            n = mangsinhvien -> n;
+            sinhvien = mangsinhvien -> sinhvien;
+        }
+
+        void set(){
+            std::cin >> n;
+            sinhvien = new SinhVien[n];
+        }
+
+        void get(){
+            std::cout << n;
+            endl;
+            for ( int i = 0; i < n; ++i){
+                std::cout << *(sinhvien + i);
+                endl;
+            }
+        }
+
+        friend std::istream& operator>> ( std::istream& in, MangSinhVien& mangsinhvien){
+            for ( int i = 0; i < mangsinhvien.n; ++i){
+                in >> *(mangsinhvien.sinhvien + i);
+                in.ignore();
+            }
+            return in;
+        }
+
+        friend std::ostream& operator<< (std::ostream& out, MangSinhVien mangsinhvien){
+            for ( int i = 0; i < mangsinhvien.n; ++i){
+                out << *(mangsinhvien.sinhvien + i);
+                out << '\n';
+            }
+            return out;
+        }
+
+        SinhVien operator[] (int index){
+            return *(sinhvien + index);
+        }
+
+        MangSinhVien operator= ( MangSinhVien mang2){
+            n = mang2.n;
+            sinhvien = mang2.sinhvien;
+        }  
+
+        double _AVERAGE_ (){
+            double sum = 0;
+            for ( int i = 0; i < n; ++i) sum += (sinhvien + i) -> get_average_mark();
+            return sum / n;
+        }
+
+        int _MAX_AVERAGE_MARK_(){
+            double max_average = _AVERAGE_();
+            int index = 0;
+            for ( int i = 0; i < n; ++i){
+               if ( max_average < (sinhvien + i) -> get_average_mark()){
+                    max_average = (sinhvien + i) -> get_average_mark();
+                    index = i;
+               }
+            }
+            return index;
+        }
+};
+
+int main (){
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr); 
+    std::cout.tie(nullptr);
+    int n;
+    std::cin >> n;
+    MangSinhVien msv (n);
+    std::cin >> msv;
+    std::cout << msv._AVERAGE_();
+    return 0;
+}
